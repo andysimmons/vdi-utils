4f6a2a6747804caa5d2eba136ddbe570dbbefbe4
Andy Simmons <ajsimmons@gmail.com>
2019-10-02 09:39:40 -0600

    Update Get-UsersWithoutHomeDrives.ps1

--- Get-UsersWithoutHomeDrives.ps1
+++ Get-UsersWithoutHomeDrives.ps1
@@ -1,5 +1,5 @@
 [CmdletBinding()]
-param(
+param (
     [IO.FileInfo]
     $OutFile = "C:\Temp\NoHomeDriveUsers.csv"
 )
@@ -10,16 +10,15 @@ $i = 0
 $homelessCount = 0
 
 # Clean up stale CSV if we have one
-if (Test-Path $OutFile) 
-{
-    Remove-Item $OutFile -Force -ErrorAction Stop
-}
+if (Test-Path $OutFile) { Remove-Item $OutFile -Force -ErrorAction Stop }
 
 # Build some collections we can use to perform some analysis without re-querying remote resources again
 Write-Verbose "[$(Get-Date -f G)] Querying AD users (this might take a minute)..."
 
-# Run the long/slow queries in parallel (as background jobs)
+# Clean up any previous jobs
 Get-Job -Name "HOMECHECK*" | Remove-Job -Force
+
+# Run the long/slow queries in parallel (as background jobs)
 Start-Job -Name 'HOMECHECK-GeneralUsers' { Get-ADUser -SearchBase "OU=General Users,OU=Testing User GPOs,OU=SL1 Users,DC=SL1,DC=STLUKES-INT,DC=ORG" -Filter * } > $null
 Start-Job -Name 'HOMECHECK-Contractors' { Get-ADUser -SearchBase "OU=Contractors,OU=Testing User GPOs,OU=SL1 Users,DC=SL1,DC=STLUKES-INT,DC=ORG" -Filter * } > $null
 Start-Job -Name 'HOMECHECK-HomeDirs' { (Get-ChildItem "\\home.slhs.org\home\" -ErrorAction Stop).Name } > $null
@@ -45,26 +44,20 @@ Discovered objects summary:
 "@
 
 # if any of these collections are empty, bail out
-if (!($generalUsers -and $contractors -and $homeFolderNames -and $vdiMembers))
-{
+if (!($generalUsers -and $contractors -and $homeFolderNames -and $vdiMembers)) {
     Write-Warning $summary
     throw "Collection initialization failed. Aborting."
 }
-else
-{
-    Write-Verbose $summary
-}
+else { Write-Verbose $summary }
 
 $adUsers = $generalUsers + $contractors
 Write-Verbose "[$(Get-Date -f G)] Analyzing $($adUsers.length) users..."
 
-
-foreach ($adUser in $adUsers)
-{
+foreach ($adUser in $adUsers) {
     $i++
     $progressParams = @{
-        Activity = "Checking VDI home drives"
-        Status = "${i}/$($adUsers.Length) (${homelessCount} homeless users found so far)"
+        Activity        = "Checking VDI home drives"
+        Status          = "${i}/$($adUsers.Length) (${homelessCount} homeless users found so far)"
         PercentComplete = 100 * $i / $adUsers.Length
     }
     Write-Progress @progressParams
@@ -73,25 +66,30 @@ foreach ($adUser in $adUsers)
     $userDN = $adUser.DistinguishedName # Helps for determining VDI group membership
 
     # If they're in a VDI Group and don't have a home drive...
-    If (($userDN -in $vdiMembers) -and ($userCN -notin $homeFolderNames))
-    {
+    If (($userDN -in $vdiMembers) -and ($userCN -notin $homeFolderNames)) {
         $homelessCount++
-        $adUser | Select-Object -Property SamAccountName,Enabled,Name,ObjectClass,DistinguishedName | 
-            Export-Csv $OutFile -Append -NoTypeInformation #Add the user's details to the report 
+        $adUser | Select-Object -Property SamAccountName, Enabled, Name, ObjectClass, DistinguishedName | 
+        Export-Csv $OutFile -Append -NoTypeInformation #Add the user's details to the report 
     }
 }
 Write-Progress -Activity "Checking VDI home drives" -Completed
 
-If (Test-Path $OutFile) #If a report was created (or, rather, if any users w/o home drives were found)
-{
-    #Send an email to the proper staff via the EXMBX1 mail/SMTP server.  Can only be run on RES AM Agent Servers that are whitelisted with Exchange.
-    #Send-MailMessage -From "HomeDriveChecker@slhs.org" -To "schneial@slhs.org" -Subject "VDI Users Without Home Drives - Report Attached" -Body "See attached CSV file." -Attachments "C:\Temp\NoHomeDriveUsers.csv" -SmtpServer MAILGATE.SLHS.ORG
+If (Test-Path $OutFile) { 
+    #If a report was created (meaning users w/o home drives were found)
     
-    # Andy testing something..
-    Invoke-Item $OutFile
+    # Send an email to the proper staff via the EXMBX1 mail/SMTP server.  Can only be run on RES AM Agent Servers 
+    # that are whitelisted with Exchange.
+    $smmParams = @{
+        From        = 'HomeDriveChecker@slhs.org'
+        To          = 'schneial@slhs.org'
+        Subject     = 'VDI Users Without Home Drives - Report Attached'
+        Body        = 'See attached CSV file.'
+        Attachments = $OutFile
+        SmtpServer  = 'mailgate.slhs.org'
+    }
+    Send-MailMessage @smmParams
     
-    #Delete the report for the next script pass so an email won't be unnecessarily sent with an old list
-    #Remove-Item $OutFile -Force
+    Remove-Item $OutFile -Force
 }
 
 $endDate = Get-Date
