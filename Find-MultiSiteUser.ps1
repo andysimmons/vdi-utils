9a97e3609dbe43eb7b38aee72fbc4661c1d81d1f
Andy Simmons <ajsimmons@gmail.com>
2020-10-14 16:38:48 -0600

    Create Find-MultiSiteUser.ps1

---
+++ Find-MultiSiteUser.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]
+param (
+    [string]
+    $SiteA = 'ctxddc01',
+
+    [string]
+    $SiteB = 'sltctxddc01',
+
+    [string]
+    $DeliveryGroup = 'XD*P10SLHS*'
+)
+Add-PSSnapin -Name Citrix.Broker.Admin.V2 -ErrorAction 'Stop'
+
+# there's definitey a better way to do this, this is just super quick and dirty
+$gbsParams = @{
+    AdminAddress     = $SiteA
+    MaxRecordCount   = [int32]::MaxValue
+    DesktopGroupName = $DeliveryGroup
+}
+$aUsers = (Get-BrokerSession @gbsParams).UserUPN
+
+$gbsParams.AdminAddress = $SiteB
+$bUsers = (Get-BrokerSession @gbsParams).UserUPN
+
+$multiSiteUsers = $aUsers.Where( { $_ -in $bUsers } )
+
+$aSite = Get-BrokerSite -AdminAddress $SiteA
+$bSite = Get-BrokerSite -AdminAddress $SiteB
+
+'*** {0} users found with a "{1}" session in both the "{2}" and "{3}" sites ***' -f $multiSiteUsers.Count, $DeliveryGroup, $aSite.Name, $bSite.Name
+$multiSiteUsers | Sort-Object
